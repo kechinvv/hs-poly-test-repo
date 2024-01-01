77ff340348653e2d0d4a9da1c411d6eaa0391085
kechin.vv <kechinv1@rambler.ru>
2024-01-01 23:31:29 +0300

    less brackets and formatting

--- src/Part1/Tasks.hs
+++ src/Part1/Tasks.hs
@@ -3,7 +3,12 @@ module Part1.Tasks where
 import Util(notImplementedYet)
 import Debug.Trace
 
-to2Pi x = if x >= 2*pi then to2Pi (x - 2*pi) else if x <= -2*pi then to2Pi (x + 2*pi) else x
+to2Pi x = if x >= 2*pi 
+          then to2Pi (x - 2*pi) 
+          else 
+              if x <= -2*pi 
+              then to2Pi (x + 2*pi) 
+              else x
 
 -- синус числа (формула Тейлора)
 mySin :: Double -> Double
@@ -12,9 +17,11 @@ mySin x = getSin x' x' (x'*x'*x') 1 6 0.0000001 x'
 
 -- getSin res s nx k f e x | trace ("getSin res=" ++ show res ++ " x=" ++ show x ++ " nx=" ++ show nx ++ " s=" ++ show s ++ " k=" ++ show k ++ " f=" ++ show f) False = undefined
 
-getSin res s nx k f e x = if abs s < e then res else getSin (res + ns) ns (nx*x*x) nk (f*(2*nk)*(2*nk+1)) e x
-        where ns = ((fromIntegral (myPow (-1) k) ) * nx) / (fromIntegral f)
-              nk = k + 1
+getSin res s nx k f e x = if abs s < e 
+                          then res
+                          else getSin (res + ns) ns (nx*x*x) nk (f*(2*nk)*(2*nk+1)) e x
+                              where ns = ((fromIntegral (myPow (-1) k) ) * nx) / fromIntegral f
+                                    nk = k + 1
               
 
 -- косинус числа (формула Тейлора)
@@ -22,27 +29,30 @@ myCos :: Double -> Double
 myCos x = getCos 1 1 (x'*x') 1 2 0.0000001 x'
             where x' = to2Pi x
 
-getCos res s nx k f e x = if abs s < e then res else getCos (res + ns) ns (nx*x*x) nk (f*(2*nk)*(2*nk-1)) e x
-        where ns = ((fromIntegral (myPow (-1) k) ) * nx) / (fromIntegral f)
-              nk = k + 1
+getCos res s nx k f e x = if abs s < e 
+                          then res 
+                          else getCos (res + ns) ns (nx*x*x) nk (f*(2*nk)*(2*nk-1)) e x
+                              where ns = ((fromIntegral (myPow (-1) k) ) * nx) / fromIntegral f
+                                    nk = k + 1
 
 -- наибольший общий делитель двух чисел
 myGCD :: Integer -> Integer -> Integer
 myGCD a 0  =  abs a
-myGCD a b  =  myGCD (abs b) (abs (a `rem` b))
+myGCD a b  =  myGCD (abs b) $ abs $ a `rem` b
 
 -- является ли дата корректной с учётом количества дней в месяце и
 -- вискокосных годов?
 isDateCorrect :: Integer -> Integer -> Integer -> Bool
-isDateCorrect d 02 y = if (mod y 400 == 0  || mod y 100 /= 0 && mod y 4 == 0)
-                            then d > 0 && d < 30
-                            else d > 0 && d < 29
+isDateCorrect d 02 y = if mod y 400 == 0  || mod y 100 /= 0 && mod y 4 == 0
+                       then d > 0 && d < 30
+                       else d > 0 && d < 29
 
-isDateCorrect d m y = if (elem m [1, 3, 5, 7, 8, 10, 12]) 
+isDateCorrect d m y = if elem m [1, 3, 5, 7, 8, 10, 12]
                         then d > 0 && d < 32 
-                        else if (elem m [2, 4, 6, 9, 11])              
-                        then d > 0 && d < 31
-                        else False 
+                        else 
+                              if elem m [2, 4, 6, 9, 11]             
+                              then d > 0 && d < 31
+                              else False 
 
 -- возведение числа в степень, duh
 -- готовые функции и плавающую арифметику использовать нельзя
@@ -54,9 +64,14 @@ myPow x y = x * myPow x (y-1)
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
 isPrime 1 = False
-isPrime x = isPrimeIter 2 x (div x 2 + 1)
+isPrime x = isPrimeIter 2 x $ div x 2 + 1
 
-isPrimeIter i x e = if (i == e) then True else if (mod x i == 0) then False else isPrimeIter (i+1) x e 
+isPrimeIter i x e = if i == e 
+                    then True 
+                    else 
+                        if mod x i == 0 
+                        then False 
+                        else isPrimeIter (i+1) x e 
 
 type Point2D = (Double, Double)
 
@@ -76,5 +91,13 @@ shapeSum x y res ((cx, cy) : points) x1 y1 = shapeSum cx cy (res + x*cy - cx*y) 
 --  2, если он прямоугольный
 --  -1, если это не треугольник
 triangleKind :: Double -> Double -> Double -> Integer
-triangleKind a b c = if (a + b < c || a + c < b || b + c < a) then -1 else getTriangleKind ((a^2 + b^2 - c^2)/(2*a*b))  ((a^2 + c^2 - b^2)/(2*a*c))  ((b^2 + c^2 - a^2)/(2*c*b))
-getTriangleKind a b c = if (a == 0 || b == 0 || c == 0) then 2 else if (a < 0 || b < 0 || c < 0) then 0 else 1
+triangleKind a b c = if a + b < c || a + c < b || b + c < a 
+                     then -1 
+                     else getTriangleKind ((a^2 + b^2 - c^2)/(2*a*b))  ((a^2 + c^2 - b^2)/(2*a*c))  ((b^2 + c^2 - a^2)/(2*c*b))
+
+getTriangleKind a b c = if a == 0 || b == 0 || c == 0 
+                        then 2 
+                        else 
+                              if a < 0 || b < 0 || c < 0 
+                              then 0 
+                              else 1
